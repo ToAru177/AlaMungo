1e2f61fb5ce3439def8475dbe1b987781a191bdc
해송 최 <rbghf@naver.com>
2019-06-24 09:19:51 +0900

    기본 셋팅

--- AlaMungo.Employee/Controls/InvoiceListControl.Designer.cs
+++ AlaMungo.Employee/Controls/InvoiceListControl.Designer.cs
@@ -30,48 +30,41 @@
         {
             this.components = new System.ComponentModel.Container();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.buyIDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.orderDateDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.invoiceStateDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.bdsBuy = new System.Windows.Forms.BindingSource(this.components);
             this.lblCount = new System.Windows.Forms.Label();
+            this.buyIDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.BuyerID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.UsedBooks = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.orderDateDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.invoiceStateDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.bdsBuy)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
             this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.buyIDDataGridViewTextBoxColumn,
+            this.BuyerID,
+            this.UsedBooks,
             this.orderDateDataGridViewTextBoxColumn,
             this.invoiceStateDataGridViewTextBoxColumn});
             this.dataGridView1.DataSource = this.bdsBuy;
             this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dataGridView1.Location = new System.Drawing.Point(0, 0);
             this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
             this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
             this.dataGridView1.Size = new System.Drawing.Size(589, 384);
             this.dataGridView1.TabIndex = 0;
-            // 
-            // buyIDDataGridViewTextBoxColumn
-            // 
-            this.buyIDDataGridViewTextBoxColumn.DataPropertyName = "BuyID";
-            this.buyIDDataGridViewTextBoxColumn.HeaderText = "배송번호";
-            this.buyIDDataGridViewTextBoxColumn.Name = "buyIDDataGridViewTextBoxColumn";
-            // 
-            // orderDateDataGridViewTextBoxColumn
-            // 
-            this.orderDateDataGridViewTextBoxColumn.DataPropertyName = "OrderDate";
-            this.orderDateDataGridViewTextBoxColumn.HeaderText = "구매일자";
-            this.orderDateDataGridViewTextBoxColumn.Name = "orderDateDataGridViewTextBoxColumn";
-            // 
-            // invoiceStateDataGridViewTextBoxColumn
-            // 
-            this.invoiceStateDataGridViewTextBoxColumn.DataPropertyName = "InvoiceState";
-            this.invoiceStateDataGridViewTextBoxColumn.HeaderText = "배송상태";
-            this.invoiceStateDataGridViewTextBoxColumn.Name = "invoiceStateDataGridViewTextBoxColumn";
             // 
             // bdsBuy
             // 
@@ -86,6 +79,41 @@
             this.lblCount.Size = new System.Drawing.Size(29, 12);
             this.lblCount.TabIndex = 1;
             this.lblCount.Text = "대기";
+            // 
+            // buyIDDataGridViewTextBoxColumn
+            // 
+            this.buyIDDataGridViewTextBoxColumn.DataPropertyName = "BuyID";
+            this.buyIDDataGridViewTextBoxColumn.HeaderText = "배송번호";
+            this.buyIDDataGridViewTextBoxColumn.Name = "buyIDDataGridViewTextBoxColumn";
+            this.buyIDDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // BuyerID
+            // 
+            this.BuyerID.DataPropertyName = "BuyerID";
+            this.BuyerID.HeaderText = "구매자 ID";
+            this.BuyerID.Name = "BuyerID";
+            this.BuyerID.ReadOnly = true;
+            // 
+            // UsedBooks
+            // 
+            this.UsedBooks.DataPropertyName = "UsedBooks";
+            this.UsedBooks.HeaderText = "책 이름";
+            this.UsedBooks.Name = "UsedBooks";
+            this.UsedBooks.ReadOnly = true;
+            // 
+            // orderDateDataGridViewTextBoxColumn
+            // 
+            this.orderDateDataGridViewTextBoxColumn.DataPropertyName = "OrderDate";
+            this.orderDateDataGridViewTextBoxColumn.HeaderText = "구매일자";
+            this.orderDateDataGridViewTextBoxColumn.Name = "orderDateDataGridViewTextBoxColumn";
+            this.orderDateDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // invoiceStateDataGridViewTextBoxColumn
+            // 
+            this.invoiceStateDataGridViewTextBoxColumn.DataPropertyName = "InvoiceState";
+            this.invoiceStateDataGridViewTextBoxColumn.HeaderText = "배송상태";
+            this.invoiceStateDataGridViewTextBoxColumn.Name = "invoiceStateDataGridViewTextBoxColumn";
+            this.invoiceStateDataGridViewTextBoxColumn.ReadOnly = true;
             // 
             // InvoiceListControl
             // 
@@ -105,10 +133,12 @@
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn buyIDDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn orderDateDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn invoiceStateDataGridViewTextBoxColumn;
         private System.Windows.Forms.BindingSource bdsBuy;
         private System.Windows.Forms.Label lblCount;
+        private System.Windows.Forms.DataGridViewTextBoxColumn buyIDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn BuyerID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn UsedBooks;
+        private System.Windows.Forms.DataGridViewTextBoxColumn orderDateDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn invoiceStateDataGridViewTextBoxColumn;
     }
 }
--- AlaMungo.Employee/EmployeeInvoice.Designer.cs
+++ AlaMungo.Employee/EmployeeInvoice.Designer.cs
@@ -29,48 +29,11 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.EmployeeInvoiceGrid = new System.Windows.Forms.DataGridView();
-            this.bdsInvoice = new System.Windows.Forms.BindingSource(this.components);
             this.invoiceSearchControl1 = new AlaMungo.Employee.Controls.InvoiceSearchControl();
-            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.BuyerID = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.UsedBooks = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.OrderDate = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.InvoiceState = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            ((System.ComponentModel.ISupportInitialize)(this.EmployeeInvoiceGrid)).BeginInit();
+            this.invoiceListControl1 = new AlaMungo.Employee.Controls.InvoiceListControl();
+            this.bdsInvoice = new System.Windows.Forms.BindingSource(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.bdsInvoice)).BeginInit();
             this.SuspendLayout();
-            // 
-            // EmployeeInvoiceGrid
-            // 
-            this.EmployeeInvoiceGrid.AllowUserToAddRows = false;
-            this.EmployeeInvoiceGrid.AllowUserToDeleteRows = false;
-            this.EmployeeInvoiceGrid.AllowUserToResizeColumns = false;
-            this.EmployeeInvoiceGrid.AllowUserToResizeRows = false;
-            this.EmployeeInvoiceGrid.AutoGenerateColumns = false;
-            this.EmployeeInvoiceGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.EmployeeInvoiceGrid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.Column1,
-            this.BuyerID,
-            this.UsedBooks,
-            this.OrderDate,
-            this.InvoiceState});
-            this.EmployeeInvoiceGrid.Cursor = System.Windows.Forms.Cursors.Default;
-            this.EmployeeInvoiceGrid.DataSource = this.bdsInvoice;
-            this.EmployeeInvoiceGrid.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.EmployeeInvoiceGrid.Location = new System.Drawing.Point(0, 90);
-            this.EmployeeInvoiceGrid.Name = "EmployeeInvoiceGrid";
-            this.EmployeeInvoiceGrid.ReadOnly = true;
-            this.EmployeeInvoiceGrid.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
-            this.EmployeeInvoiceGrid.RowTemplate.Height = 23;
-            this.EmployeeInvoiceGrid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
-            this.EmployeeInvoiceGrid.Size = new System.Drawing.Size(800, 360);
-            this.EmployeeInvoiceGrid.StandardTab = true;
-            this.EmployeeInvoiceGrid.TabIndex = 1;
-            // 
-            // bdsInvoice
-            // 
-            this.bdsInvoice.DataSource = typeof(AlaMungo.Data.Buy);
             // 
             // invoiceSearchControl1
             // 
@@ -80,52 +43,24 @@
             this.invoiceSearchControl1.Size = new System.Drawing.Size(800, 90);
             this.invoiceSearchControl1.TabIndex = 0;
             // 
-            // Column1
+            // invoiceListControl1
             // 
-            this.Column1.DataPropertyName = "BuyID";
-            this.Column1.HeaderText = "배송 ID";
-            this.Column1.Name = "Column1";
-            this.Column1.ReadOnly = true;
-            // 
-            // BuyerID
-            // 
-            this.BuyerID.DataPropertyName = "BuyerID";
-            this.BuyerID.HeaderText = "구매자 ID";
-            this.BuyerID.Name = "BuyerID";
-            this.BuyerID.ReadOnly = true;
-            // 
-            // UsedBooks
-            // 
-            this.UsedBooks.DataPropertyName = "UsedBooks";
-            this.UsedBooks.HeaderText = "책 이름";
-            this.UsedBooks.Name = "UsedBooks";
-            this.UsedBooks.ReadOnly = true;
-            // 
-            // OrderDate
-            // 
-            this.OrderDate.DataPropertyName = "OrderDate";
-            this.OrderDate.HeaderText = "구매 일자";
-            this.OrderDate.Name = "OrderDate";
-            this.OrderDate.ReadOnly = true;
-            // 
-            // InvoiceState
-            // 
-            this.InvoiceState.DataPropertyName = "InvoiceState";
-            this.InvoiceState.HeaderText = "배송 상태";
-            this.InvoiceState.Name = "InvoiceState";
-            this.InvoiceState.ReadOnly = true;
+            this.invoiceListControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.invoiceListControl1.Location = new System.Drawing.Point(0, 90);
+            this.invoiceListControl1.Name = "invoiceListControl1";
+            this.invoiceListControl1.Size = new System.Drawing.Size(800, 360);
+            this.invoiceListControl1.TabIndex = 1;
             // 
             // EmployeeInvoice
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.EmployeeInvoiceGrid);
+            this.Controls.Add(this.invoiceListControl1);
             this.Controls.Add(this.invoiceSearchControl1);
             this.Name = "EmployeeInvoice";
             this.Text = "배송관리";
             this.Load += new System.EventHandler(this.EmployeeInvoice_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.EmployeeInvoiceGrid)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.bdsInvoice)).EndInit();
             this.ResumeLayout(false);
 
@@ -134,16 +69,11 @@
         #endregion
 
         private Controls.InvoiceSearchControl invoiceSearchControl1;
-        private System.Windows.Forms.DataGridView EmployeeInvoiceGrid;
-        private System.Windows.Forms.BindingSource bdsInvoice;
         private System.Windows.Forms.DataGridViewTextBoxColumn buyIDDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn buyerIDDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn orderDateDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn invoiceStateDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn BuyerID;
-        private System.Windows.Forms.DataGridViewTextBoxColumn UsedBooks;
-        private System.Windows.Forms.DataGridViewTextBoxColumn OrderDate;
-        private System.Windows.Forms.DataGridViewTextBoxColumn InvoiceState;
+        private Controls.InvoiceListControl invoiceListControl1;
+        private System.Windows.Forms.BindingSource bdsInvoice;
     }
 }
